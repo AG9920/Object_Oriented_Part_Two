60e6854c3b75eddc2cb881a565eb3462290d2091
AG9920 <futuregong@126.com>
2016-06-06 16:43:42 +0800

    new

--- src/com/futuregong/crazy_Java/AutoBoxingUnboxing.java
+++ src/com/futuregong/crazy_Java/AutoBoxingUnboxing.java
@@ -2,6 +2,11 @@ package com.futuregong.crazy_Java;
 
 //自动装箱，拆箱示范
 
+/*
+ * 自动装箱：把一个基本类型变量直接赋给对应的包装类变量，或者赋给Object变量（所有类的父类，子类对象课直接赋给父类变量）
+ * 自动拆箱：直接把包装类对象直接赋给一个对应的基本类型变量
+ */
+
 public class AutoBoxingUnboxing {
 	public static void main(String[] args) {
 		//直接把一个基本类型变量赋给Integer对象
---
+++ src/com/futuregong/crazy_Java/Primitive2String.java
@@ -0,0 +1,35 @@
+package com.futuregong.crazy_Java;
+
+//基本类型变量和字符串之间的转换
+
+public class Primitive2String {
+	
+	public static void main(String[] args) {
+		String intStr = "1123";
+		//把一个特定字符串转换成int变量
+		int it = Integer.parseInt(intStr);
+		System.out.println(it);
+		
+		
+		String floatStr = "4.56";
+		//把一个特定字符串转换成float变量
+		float ft = Float.parseFloat(floatStr);
+		System.out.println(ft);
+		
+		
+		//把一个float变量转换成String变量
+		String ftStr = String.valueOf(2.34f);
+		System.out.println(ftStr);
+		
+		
+		//把一个double变量转换成String变量
+		String dbStr = String.valueOf(3.344);
+		System.out.println(dbStr);
+		
+		//把一个boolean变量转换成String变量
+		String boolStr = String.valueOf(true);
+		System.out.println(boolStr.toUpperCase());
+	}
+
+}
+
